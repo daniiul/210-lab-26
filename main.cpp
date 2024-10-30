01391d0dd06aba85ce724fe098331b437b2ac3d6
daniiul <dmalakhov2005@gmail.com>
2024-10-30 11:09:36 -0700

    abstracted a bunch of code now making print statements

--- main.cpp
+++ main.cpp
@@ -12,14 +12,12 @@
 using namespace std;
 using namespace std::chrono;
 
-void readFile(int, int, int(&data)[15][4][3]);
-void sortFile(int, int, int(&data)[15][4][3]);
-void insertFile(int, int, int(&data)[15][4][3]);
-void deleteFile(int, int, int(&data)[15][4][3]);
+void readFile(int, int, int(&data)[15][4][3], vector<string>&, list<string>&, set<string>&);
+void sortFile(int, int, int(&data)[15][4][3], vector<string>&, list<string>&, set<string>&);
+void insertFile(int, int, int(&data)[15][4][3], vector<string>&, list<string>&, set<string>&);
+void deleteFile(int, int, int(&data)[15][4][3], vector<string>&, list<string>&, set<string>&);
 
 int main() {
-
-
 
     int data[15][4][3];
 
@@ -35,106 +33,33 @@ int main() {
     // reads all the codes in text files into vector, list, and set
     for(int i = 0; i < 15; i++)
     {
-        readFile(i, 0, data);
-        readFile(i, 1, data);
-        readFile(i, 2, data);
+        readFile(i, 0, data, vec, lis, se);
+        readFile(i, 1, data, vec, lis, se);
+        readFile(i, 2, data, vec, lis, se);
     }
 
     // sorts the vectors and list
-    for(int i = 0; i < 3; i++)
+    for(int i = 0; i < 15; i++)
     {
-        // Start timing
-        auto start = high_resolution_clock::now();
-
-        // Example loop to measure
-        if (i == 0)
-        {
-            sort(vec.begin(), vec.end());
-        }
-        if (i == 1)
-        {
-
-            lis.sort();
-        }
-        if (i == 2)
-        {
-            timeSort.push_back(-1);
-        }
-
-        // End timing
-        auto end = high_resolution_clock::now();
-
-        // Calculate duration
-        auto duration = duration_cast<microseconds>(end - start);
-
-        timeSort.push_back(duration.count());
+        sortFile(i, 0, data, vec, lis, se);
+        sortFile(i, 1, data, vec, lis, se);
+        sortFile(i, 2, data, vec, lis, se);
     }
 
     // inserts an element into middle of vector, list, set
-    for(int i = 0; i < 3; i++)
+    for(int i = 0; i < 15; i++)
     {
-        // Start timing
-        auto start = high_resolution_clock::now();
-
-        // Example loop to measure
-        if (i == 0)
-        {
-            vec.insert(vec.begin() + 10000, "TESTCODE");
-        }
-        if (i == 1)
-        {
-            auto it = lis.begin();
-            advance(it, 10000);
-            lis.insert(it, "TESTCODE");
-        }
-        if (i == 2)
-        {
-            se.insert("TESTCODE");
-        }
-
-        // End timing
-        auto end = high_resolution_clock::now();
-
-        // Calculate duration
-        auto duration = duration_cast<microseconds>(end - start);
-
-        timeInsert.push_back(duration.count());
+        insertFile(i, 0, data, vec, lis, se);
+        insertFile(i, 1, data, vec, lis, se);
+        insertFile(i, 2, data, vec, lis, se);
     }
 
     // deletes an element from vector, list and set
-    for(int i = 0; i < 3; i++)
+    for(int i = 0; i < 15; i++)
     {
-        // Start timing
-        auto start = high_resolution_clock::now();
-
-        // Example loop to measure
-        if (i == 0)
-        {
-            auto it = vec.begin();
-            advance(it, 10001);
-            vec.erase(it);
-        }
-        if (i == 1)
-        {
-            auto it1 = lis.begin();
-            advance(it1, 10005);
-            lis.erase(it1);
-
-        }
-        if (i == 2)
-        {
-            auto it = se.begin();
-            advance(it, 10001);
-            se.erase(it);
-        }
-
-        // End timing
-        auto end = high_resolution_clock::now();
-
-        // Calculate duration
-        auto duration = duration_cast<microseconds>(end - start);
-
-        timeDelete.push_back(duration.count());
+        deleteFile(i, 0, data, vec, lis, se);
+        deleteFile(i, 1, data, vec, lis, se);
+        deleteFile(i, 2, data, vec, lis, se);
     }
 
     cout << setw(10) << right << "Operation" << setw(10) << right << "Vector" << setw(10) << right << "List" << setw(10) << right << "Set" << endl;
@@ -148,7 +73,7 @@ int main() {
     return 0;
 }
 
-void readFile(int run, int i, int(&data)[15][4][3])
+void readFile(int run, int i, int(&data)[15][4][3], vector<string>& vec, list<string>& lis, set<string>& se)
 {
 
     ifstream inputFile("codes.txt");
@@ -194,6 +119,101 @@ void readFile(int run, int i, int(&data)[15][4][3])
     inputFile.seekg(0);
 }
 
+void sortFile(int run, int i, int(&data)[15][4][3], vector<string>& vec, list<string>& lis, set<string>& se)
+{
+    // Start timing
+    auto start = high_resolution_clock::now();
+
+    // Example loop to measure
+    if (i == 0)
+    {
+        sort(vec.begin(), vec.end());
+    }
+    if (i == 1)
+    {
+
+        lis.sort();
+    }
+
+
+    // End timing
+    auto end = high_resolution_clock::now();
+
+    // Calculate duration
+    auto duration = duration_cast<microseconds>(end - start);
+
+    data[run][1][i] = duration.count();
+    if (i == 2)
+    {
+        data[run][1][i] = -1;
+    }
+
+}
+
+void insertFile(int run, int i, int(&data)[15][4][3], vector<string>& vec, list<string>& lis, set<string>& se)
+{
+    // Start timing
+    auto start = high_resolution_clock::now();
+
+    // Example loop to measure
+    if (i == 0)
+    {
+        vec.insert(vec.begin() + 10000, "TESTCODE");
+    }
+    if (i == 1)
+    {
+        auto it = lis.begin();
+        advance(it, 10000);
+        lis.insert(it, "TESTCODE");
+    }
+    if (i == 2)
+    {
+        se.insert("TESTCODE");
+    }
+
+    // End timing
+    auto end = high_resolution_clock::now();
+
+    // Calculate duration
+    auto duration = duration_cast<microseconds>(end - start);
+
+    data[run][2][i] = duration.count();
+}
+
+void deleteFile(int run, int i, int(&data)[15][4][3], vector<string>& vec, list<string>& lis, set<string>& se)
+{
+    // Start timing
+    auto start = high_resolution_clock::now();
+
+    // Example loop to measure
+    if (i == 0)
+    {
+        auto it = vec.begin();
+        advance(it, 10001);
+        vec.erase(it);
+    }
+    if (i == 1)
+    {
+        auto it1 = lis.begin();
+        advance(it1, 10005);
+        lis.erase(it1);
+
+    }
+    if (i == 2)
+    {
+        auto it = se.begin();
+        advance(it, 10001);
+        se.erase(it);
+    }
+
+    // End timing
+    auto end = high_resolution_clock::now();
+
+    // Calculate duration
+    auto duration = duration_cast<microseconds>(end - start);
+
+    data[run][3][i] = duration.count();
+}
 /* syntax examples:
 auto start = high_resolution_clock::now()
 auto end = high_resolution_clock::now()
